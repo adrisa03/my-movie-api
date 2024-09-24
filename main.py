57c55ebb1a6e02c18b76df36648f4d56d120d392
Adriana Torres <aditorn@ymail.com>
2024-09-24 16:43:49 -0500

    Abordamos la seccion 2 fast api

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI #importa la clase fastapi de la libreria fastapi
-
+from fastapi.responses import HTMLResponse #importa la clase HTMLResponse de la libreria fastapi
+from movies_list import movies_list
 app = FastAPI() #crea una instancia de la clase fastapi
 app.title = 'Mi primer aplicacion de peliculas y analisis de datos'
 app.version = '0.0.1' #asigna un valor a la propiedad title de la clase fastapi
 @app.get('/', tags=['Home']) #definiendo una ruta
 def message(): #definiendo una funcion de la ruta
-    return 'Hello World' #devolviendo un valor
+    return HTMLResponse('<h1>Hello World</h1>') #retorna un objeto de la clase HTMLResponse
+    
+@app.get('/movies', tags=['Movies']) #definiendo una ruta de la clase fast api
+def movies(): #definiendo una ruta
+    return movies_list
+
+@app.get('/movies/{id}', tags=['Movies']) #app get consultar por id
+def get_movie(id:int): #
+    for item in movies_list:
+        if item['id'] == id:
+            return item
+    return []
---
+++ movies_list.py
@@ -0,0 +1,72 @@
+movies_list = [
+
+    {   "id": 1,
+        "title": "The Shawshank Redemption",
+        "overwiew": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+        "year": 1994,
+        "rating": 9.3
+    },
+
+    {   "id": 2,
+        "title": "The Godfather",
+        "overwiew": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        "year": 1972,
+        "rating": 9.2
+    },
+
+    {   "id": 3,
+        "title": "The Godfather: Part II",
+        "overwiew": "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.", 
+        "year": 1974,
+        "rating": 9.0
+    },
+
+    {   "id": 4,
+        "title": "The Dark Knight", 
+        "overwiew": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological tests of his ability to fight injustice.",
+        "year": 2008,
+        "rating": 9.0
+    },
+
+    {   "id": 5,
+        "title": "12 Angry Men",
+        "overwiew": "The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces them to more carefully consider the evidence before filing aplaint",  
+        "year": 1957,
+        "rating": 8.9
+    },
+
+    {   "id": 6,
+        "title": "Schindler's List",    
+        "overwiew": "In Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.", 
+        "year": 1993,
+        "rating": 8.9
+    },
+
+    {   "id": 7,
+        "title": "The Lord of the Rings: The Return of the King",
+        "overwiew": "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.", 
+        "year": 2003,
+        "rating": 8.9
+    },
+
+    {   "id": 8,
+        "title": "Pulp Fiction",
+        "overwiew": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",  
+        "year": 1994,
+        "rating": 8.9
+    },
+
+    {   "id": 9, 
+        "title": "The Good, the Bad and the Ugly",
+        "overwiew": "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.", 
+        "year": 1966,
+        "rating": 8.8
+    },
+
+    {   "id": 10,
+        "title": "The Lord of the Rings: The Fellowship of the Ring",
+        "overwiew": "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.", 
+        "year": 2001,
+        "rating": 8.8
+    }
+]
